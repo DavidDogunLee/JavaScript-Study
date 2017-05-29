4c0b0b205a8c3c57ea2cbd70814684fbd13a22c2
John Lee <johnjaehoonlee@gmail.com>
2017-05-28 20:21:00 -0700

    objects and arrays exercise

---
+++ JavaScript-objects-and-arrays/main.js
@@ -0,0 +1,26 @@
+var items = [
+  {
+    id: 0,
+    date: 'May 7, 2013',
+    name: 'Candy',
+    price: '$1.00'
+  },
+  {
+    id: 1,
+    date: 'May 2, 2013',
+    name: 'Pepsi',
+    price: '$0.99'
+  },
+  {
+    id: 2,
+    date: 'May 2, 2013',
+    name: 'Bread',
+    price: '$2.99'
+  },
+  {
+    id: 3,
+    date: 'April 29, 2013',
+    name: 'Water',
+    price: '$1.99'
+  }
+]
