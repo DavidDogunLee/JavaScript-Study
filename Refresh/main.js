b3fdf9bf9bb9e6cc08cb0fc755c736b612ecabae
John Lee <johnjaehoonlee@gmail.com>
2017-08-20 17:55:49 -0700

    refresh

---
+++ Refresh/main.js
@@ -0,0 +1,20 @@
+var food = 'pizza'
+food = 'calzone'
+
+var price = 9.99
+price += 1
+price -= 5
+
+var place = 'Greyskull!'
+var power = 'By the power of ' + place
+place = 'Eternia!'
+power = power + place
+
+var myDog = {
+  name: 'Junior',
+  breed: 'Boston Terrier',
+  age: 3,
+  isNice: true
+}
+
+myDog.sex = 'Boy';
