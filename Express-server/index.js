f10d19d2177e7f3538e3aab0647c7348d6abae47
DavidLee8two7 <DavidLee8two7@gmail.com>
2017-10-16 23:46:49 -0700

    Revision

--- Express-server/index.js
+++ Express-server/index.js
@@ -1,23 +1,7 @@
 var express = require('express')
 
-// Middleware to print the unix timestamp of the request.
-var logTimestamp = (req, res, next) => {
-  console.log('Request received:', Date.now())
-  next()
-}
-
-// Middleware to send a 200 OK response to the client.
-var respond = (req, res) => {
-  res.sendStatus(200)
-}
-
 var app = express()
 
-// Registering our middlware
-// in the order they should process requests.
-app.use(logTimestamp)
-app.use(respond)
-
-app.listen(3000, () => {
-  console.log('Listening on port 3000!')
+app.listen(3000, ()=>{
+  console.log('listening on port 3000')
 })
