3e50f22ee23703c85ecb7c08914c80c198612f52
DavidLee8two7 <DavidLee8two7@gmail.com>
2017-10-25 00:05:05 -0700

    express sendFile

--- express-middleware/index.js
+++ express-middleware/index.js
@@ -1,47 +1,14 @@
 var express = require('express')
 var app = express()
 
-var handleGet = (req, res, next) => {
-  if (req.method === 'GET' && req.url === '/express-get') {
-    res.send('You sent a GET request!')
-  }
-  else {
-    next()
-  }
+var respond = (req, res, next) => {
+  if (req.method !== 'GET') return next()
+  var options = { root: __dirname }
+  res.sendFile(req.url, options)
 }
 
-var handlePost = (req, res, next) => {
-  if (req.method === 'POST' && req.url === '/express-post') {
-    res.send('You sent a POST request!')
-  }
-  else {
-    next()
-  }
-}
-
-var handlePut = (req, res, next) => {
-  if (req.method === 'PUT' && req.url === '/express-put') {
-    res.send('You sent a PUT request!')
-  }
-  else {
-    next()
-  }
-}
-
-var handleDelete = (req, res, next) => {
-  if (req.method === 'DELETE' && req.url === '/express-delete') {
-    res.send('You sent a DELETE request!')
-  }
-  else {
-    next()
-  }
-}
-
-app.use(handleGet)
-app.use(handlePost)
-app.use(handlePut)
-app.use(handleDelete)
+app.use(respond)
 
 app.listen(3000, () => {
-  console.log('Listening on port 3000')
+  console.log('Listening on port', 3000)
 })
