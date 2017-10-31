bf059af48f34f81cf8f21d7c3cf05e9b188a1e7f
DavidLee8two7 <DavidLee8two7@gmail.com>
2017-10-31 00:46:52 -0700

    dice game

--- express-middleware/index.js
+++ express-middleware/index.js
@@ -1,14 +1,2 @@
-var express = require('express')
+var express = require('exress')
 var app = express()
-
-var respond = (req, res, next) => {
-  if (req.method !== 'GET') return next()
-  var options = { root: __dirname }
-  res.sendFile(req.url, options)
-}
-
-app.use(respond)
-
-app.listen(3000, () => {
-  console.log('Listening on port', 3000)
-})
