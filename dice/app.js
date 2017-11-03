282d317b040aff2d20f9430f18994be65ade42f7
DavidLee8two7 <DavidLee8two7@gmail.com>
2017-11-02 22:00:01 -0700

    Dice game is finished

--- dice/app.js
+++ dice/app.js
@@ -6,66 +6,93 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
-var scores, roundScore, activePlayer;
+var scores, roundScore, activePlayer, gamePlaying;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
-// Setter
-// document.querySelector('#current-' + activePlayer).textContent = dice;
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'
-// getter
-// var x = document.querySelector('#score-0').textContent;
-
-// CSS manuplation
-document.querySelector('.dice').style.display = 'none';
-
-// Use Callback function when needs to be used morethan once
-/*function btn() {
-  // Do something here
-}
-btn();
-*/
-
-document.getElementById('score-0').textContent = '0'; // getElement method is faster. No need CSS style test style # or .
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
-// Callback function: function is pass into another function as argument
 document.querySelector('.btn-roll').addEventListener('click', function() {
-  // 1. Random number
-  var dice = Math.floor(Math.random() * 6 + 1);
-  // 2. Display the result
-  var diceDOM = document.querySelector('.dice');
-  diceDOM.style.display = 'block';
-  diceDOM.src = 'dice-' + dice + '.png';
-  // 3. Update the round score IF the rolled number was NOT a 1
-  if (dice !== 1) {
-    // Add score
-    roundScore += dice; // roundScore = roundScore + dice;
-    document.querySelector('#current-' + activePlayer).textContent = roundScore;
-  } else {
-    // Next player
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; // tenary operator
-    /* is same as
-    if (activePlayer === 0) {
-      activePlayer = 1;
+  if(gamePlaying) {
+    // 1. Random number
+    var dice = Math.floor(Math.random() * 6 + 1);
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    // 3. Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+      // Add score
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
-      activePlayer = 0;
+      // Next player
+      nextPlayer()
     }
-  */
-    roundScore = 0;
 
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-
-    // document.querySelector('.player-0-panel').classList.remove('active');
-    // document.querySelector('.player-1-panel').classList.add('active');
-    document.querySelector('.player-0-panel').classList.toggle('active'); // remove and add switches
-    document.querySelector('.player-1-panel').classList.toggle('active');
-
-    document.querySelector('.dice').style.display = 'none';
   }
+
 });
+
+function nextPlayer() {
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  if (gamePlaying) {
+    // Add current score to global score
+    scores[activePlayer] += roundScore
+
+    // update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+
+    // check if player won the game
+    if (scores[activePlayer] >= 20) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+      document.querySelector('.dice').style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      gamePlaying = false;
+    } else {
+      // next player
+      nextPlayer()
+    }
+  }
+})
+
+
+document.querySelector('.btn-new').addEventListener('click', init)
+
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gamePlaying = true;
+
+  document.querySelector('.dice').style.display = 'none';
+
+  document.getElementById('score-0').textContent = '0'; // getElement method is faster. No need CSS style test style # or .
+  document.getElementById('score-1').textContent = '0';
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.getElementById('name-0').textContent = 'Player 1';
+  document.getElementById('name-1').textContent = 'Player 2';
+
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+
+  document.querySelector('.player-0-panel').classList.remove('active');
+  document.querySelector('.player-1-panel').classList.remove('active');
+
+  document.querySelector('.player-0-panel').classList.add('active');
+}
