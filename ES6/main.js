9f22e9aec1b5b04159dd5602e27dd5233729957c
DavidLee8two7 <DavidLee8two7@gmail.com>
2017-12-10 18:49:16 -0800

    default parameter

--- ES6/main.js
+++ ES6/main.js
@@ -1,12 +1,11 @@
 // let and const
 
 // ES5
-
 // var name5 = 'Jane Smith';
 // var age5 = 23;
 // name5 = 'Jane Miller';
 // console.log(name5);
-//
+
 // // ES6
 // const name6 = 'Jane Smith';
 // let age6 = 23;
@@ -14,28 +13,58 @@
 // console.log(name6);
 
 // ES5
-function driversLicence5(passedTest) {
-  console.log(firstName)
+// function driversLicence5(passedTest) {
+//   console.log(firstName)
+//
+//   if (passedTest) {
+//     var firstName = 'John';
+//     var yearOfBirth = 1990;
+//   }
+//
+//   console.log(firstName + ', born in ' + yearOfBirth + ', is now allowed to dirve a car.')
+// }
+// driversLicence5(true);
+//
+// // ES6
+// function driversLicence6(passedTest) {
+//   console.log(firstName)
+//       let firstName;
+//       const yearOfBirth = 1990;
+//
+//   if (passedTest) {
+//     firstName = 'John';
+//   }
+//
+//   console.log(firstName + ', born in ' + yearOfBirth + ', is now allowed to dirve a car.')
+// }
+// driversLicence6(true);
 
-  if (passedTest) {
-    var firstName = 'John';
-    var yearOfBirth = 1990;
-  }
 
-  console.log(firstName + ', born in ' + yearOfBirth + ', is now allowed to dirve a car.')
-}
-driversLicence5(true);
+// Default parameter
+// ES5
+// function LeePerson(firstName, yearOfBirth, lastName, nationality) {
+//
+//   lastName === undefined ? lastName = 'Lee' : lastName = lastName;
+//   nationality === undefined ? nationality = 'Korean' : nationality = nationality;
+//
+//   this.firstName = firstName;
+//   this.yearOfBirth = yearOfBirth;
+//   this.lastName = lastName;
+//   this.nationality = nationality;
+//
+// }
+//
+// var david = new LeePerson('David', 1990);
+
 
 // ES6
-function driversLicence6(passedTest) {
-  console.log(firstName)
-      let firstName;
-      const yearOfBirth = 1990;
+function LeePerson(firstName, yearOfBirth, lastName = 'Lee', nationality = 'Korean') {
 
-  if (passedTest) {
-    firstName = 'John';
-  }
+  this.firstName = firstName;
+  this.yearOfBirth = yearOfBirth;
+  this.lastName = lastName;
+  this.nationality = nationality;
 
-  console.log(firstName + ', born in ' + yearOfBirth + ', is now allowed to dirve a car.')
-}
-driversLicence6(true);
+};
+
+var david = new LeePerson('David', 1990);
